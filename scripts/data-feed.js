d2aa706df2c4ba8b8ae6f57533285e654f30b65f
Chris Keller <christopherlawrencekeller@gmail.com>
2011-11-26 12:19:59 -0600

    Initial Commit

---
+++ scripts/data-feed.js
@@ -0,0 +1,173 @@
+//global variables
+var chart;
+var map;
+var series;
+var minChart = 0;
+var maxChart = 2100;
+var ward;
+var votesSoglin;
+var votesCieslewicz;
+var votesTotal;
+
+//begin function
+$(document).ready(function(){
+
+	//chart options
+	var optionsChart = {
+
+		chart: {
+			renderTo: 'hc-ouput',
+			defaultSeriesType: 'column',
+			marginBottom: 35,
+		},
+
+		title: {
+			style: {
+				color: '#000000',
+			},
+			align: 'left',
+			text: '',
+		},
+		subtitle: {
+			style: {
+				color: '#000000',
+			},
+			y: 37,
+			align: 'left',
+		},
+		xAxis: {
+			categories: [
+				'Total',
+				'Soglin',
+				'Cieslewicz',
+			]
+		},
+
+		yAxis: {
+			min: minChart,
+			max: maxChart,
+			title: {
+				text: 'Number of Votes',
+				style: {
+					color: '#999999',
+				}
+			}
+		},
+
+		legend: {
+			itemStyle: {},
+			layout: 'vertical',
+			backgroundColor: '#FFFFFF',
+			align: 'left',
+			verticalAlign: 'top',
+			x: 1000000,
+			y: 70,
+			floating: true,
+			shadow: true
+		},
+
+		tooltip: {
+			style: {},
+
+			formatter: function () {
+				return '' + this.point.name + ':' + '<b>' + this.y + ' votes';
+			}
+		},
+
+		plotOptions: {
+			column: {
+				pointPadding: 0.025,
+				borderWidth: 0
+			}
+		},
+
+		credits: {
+			style: {
+				color: '#666666',
+			},
+
+			text: 'SOURCE: City of Madison',
+			href: 'http://www.cityofmadison.com/',
+			position: {
+				align: 'right',
+				y: -3
+			}
+		},
+
+		series: []
+
+	};
+
+	//map options
+    var optionsMap = {
+		zoom: 11,
+		center: new google.maps.LatLng(43.083433,-89.403305),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false,
+		disableDragging: true,
+		mapTypeControl: false,
+		navigationControl: true,
+		streetViewControl: false,
+		scaleControl: false,
+		navigationControlOptions: {
+			style: google.maps.NavigationControlStyle.SMALL,
+			position: google.maps.ControlPosition.RIGHT_TOP
+		}
+	};
+	
+	//write the map
+	var map = new google.maps.Map(document.getElementById('map_canvas'), optionsMap);
+	
+	//fusion layer
+	layer = new google.maps.FusionTablesLayer(835224, {suppressInfoWindows: true});
+	layer.setMap(map);
+	
+	//click listener
+	google.maps.event.addListener(layer, 'click', function(q) {
+
+			//assign data to chart variables
+			var ward = q.row['Ward_name'].value
+			var votesSoglin = q.row['Votes_Paul_Soglin'].value
+			var votesCieslewicz = q.row['Votes_Dave_Cieslewicz'].value
+			var votesTotal = q.row['Total_ward_votes'].value
+
+			//write chart series
+			var series = {
+				data: [
+					['Ward ' + ward, votesTotal],	
+					['Ward ' + ward, votesSoglin],
+					['Ward ' + ward, votesCieslewicz],
+				]
+			};
+	
+		//write data into divs
+		$('#map-explainer').hide();
+
+		$("#ft-ouput").html( 
+		'<table>' +
+		'<h4>Ward ' + q.row['Ward_name'].value + ' (Aldermanic District ' + q.row['ald'].value + ')</h4>' +
+		'<tbody>' +
+		'<tr>' +
+		'<th>Candidate</th>' +
+		'<th>Percent</th>' +
+		'</tr>' +
+		'<tr>' +
+		'<td>Soglin</td>' +
+		'<td>' + q.row['Percent_Paul_Soglin'].value + '</td>' +
+		'</tr>' +
+		'<tr>' +
+		'<td>Cieslewicz</td>' +
+		'<td>' + q.row['Percent_Dave_Cieslewicz'].value + '</td>' +
+		'</tr>' +
+		'</tbody>' +
+		'</table>');
+
+			//push the chart series to the chart options
+			optionsChart.series.push(series);
+	
+			//write the chart
+			var chart = new Highcharts.Chart(optionsChart);
+
+	});
+
+});
