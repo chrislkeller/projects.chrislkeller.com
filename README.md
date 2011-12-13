1271523447c5e6ff0bccc91e7ec8e9e6773115ce
Chris Keller <christopherlawrencekeller@gmail.com>
2011-12-12 21:22:35 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Further stripping down the [Fusion-Tables-To-Highcharts project](https://github.com/chrislkeller/Fusion-Tables-To-Highcharts), this uses [Google Fusion Tables](http://www.google.com/fusiontables/) as a data backend to draw a chart that uses the [Highcharts.js](http://www.highcharts.com/) library.
+
+Unlike the previous project, this doesn't include a map, and therefore isn't relying on a click event to draw the chart.
+
+<p class="small"><a href="http://www.projects.chrislkeller.com/ft-as-highcharts-datasource">View the demo</a>.<br />
+<a href="https://github.com/chrislkeller/ft-as-highcharts-datasource">Fork the repo</a>.</p>
+
+Chris K.
+
+<hr />
+
+License: The MIT License
---
+++ scripts/data-feed.js
@@ -0,0 +1,122 @@
+//global variables
+var chart;
+var optionsChart;
+var minChart = 0;
+var maxChart = 90000;
+var votesTotal;
+var candidateSoglin;
+var candidateCieslewicz;
+var dataSource = 2409321;
+
+//begin function
+$(document).ready(function(){
+
+	//chart options
+	optionsChart = {
+
+		chart: {
+			renderTo: 'hc-ouput',
+			defaultSeriesType: 'column',
+			marginBottom: 35,
+		},
+
+		title: {
+			style: {
+				color: '#000000',
+			},
+			align: 'center',
+			text: '2011 Madison Mayoral Voting',
+		},
+		subtitle: {
+			style: {
+				color: '#000000',
+			},
+			y: 37,
+			align: 'left',
+		},
+		xAxis: {
+			categories: [
+				'Total Votes',
+				'Paul Soglin',
+				'Dave Cieslewicz',
+			]
+		},
+
+		yAxis: {
+			min: minChart,
+			max: maxChart,
+			title: {
+				text: 'Number of Votes',
+				style: {
+					color: '#999999',
+				}
+			}
+		},
+
+		legend: {
+			itemStyle: {},
+			layout: 'vertical',
+			backgroundColor: '#FFFFFF',
+			align: 'left',
+			verticalAlign: 'top',
+			x: 1000000,
+			y: 70,
+			floating: true,
+			shadow: true
+		},
+
+		tooltip: {
+			style: {},
+
+			formatter: function () {
+				return '<b>' + this.y + ' votes';
+			}
+		},
+
+		plotOptions: {
+			column: {
+				pointPadding: 0.025,
+				borderWidth: 0
+			}
+		},
+
+		credits: {
+			style: {
+				color: '#666666',
+			},
+
+			text: 'SOURCE: City of Madison',
+			href: 'http://www.cityofmadison.com/',
+			position: {
+				align: 'right',
+				y: -3
+			}
+		},
+
+		series: [{
+		  data: []
+		}]
+
+	};
+
+		//write data
+		$.getJSON('http://tables.googlelabs.com/api/query?sql=SELECT * FROM ' + dataSource + ' &jsonCallback=?', function(data) {
+
+	    	//assign data to chart variables
+			var votesTotal = data.table.rows[0][0]
+			var candidateSoglin = data.table.rows[0][1]
+			var candidateCieslewicz = data.table.rows[0][2]
+
+			//write chart series
+			var data = [votesTotal, candidateSoglin, candidateCieslewicz];
+	
+			// Create the chart if it doesn't exist
+			if(!chart)
+				chart = new Highcharts.Chart(optionsChart);
+	
+			// Set new data to the chart
+			chart.series[0].setData(data)
+
+		});
+
+});
