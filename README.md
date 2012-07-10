5d1c4e0d9d062f9e784c7501e998a568820e5cf0
Chris Keller <christopherlawrencekeller@gmail.com>
2012-07-10 11:24:55 -0500

    Initial Commit
    Adding base files for example project...

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-tabletop_to_datatables
+tabletop to datatables
 ======================
 
-Use tabletop.js to pull json from Google Spreadsheet and feed it to the DataTables jQuery plugin
+Use [Tabletop.js](http://builtbybalance.com/Tabletop/) to pull json from Google Spreadsheet and feed it to the [DataTables](http://datatables.net/) jQuery plugin.
+
+**Step 1** - [Prepare Google Spreadsheet data](http://builtbybalance.com/Tabletop/#tabletop-instructions).
+
+**Step 2** - Adjust tabletop_feed.js to account for spreadsheet key.
+
+    var urlStart = 'https://docs.google.com/spreadsheet/pub?';
+    var urlKey = 'key=0An8W63YKWOsxdHVreXpLbVRWUGlJUlcweHVfZ01ycVE&';
+    var urloutput = 'single=true&gid=2&output=html';
+    var public_spreadsheet_url = 	urlStart + urlKey + urloutput;
+
+**Step 3** - Set up your DataTables column headers in tabletop_feed.js.
+
+    var tableColumnSet =   [
+	{ "sTitle": "Name", "sClass": "center"},
+    	{ "sTitle": "Website", "sClass": "center"},
+    	{ "sTitle": "City", "sClass": "center"}
+    ];
+
+**Step 4** - Access the data from your spreadsheet in tabletop_feed.js.
+
+    $.each( tabletop.sheets("Active Breweries").all(), function(i, brewery) {
+    	var nameData = brewery.name;
+    	var websiteData = brewery.website;
+    	var cityData = brewery.city;
+    	var myArray = [nameData, websiteData, cityData]
+    	newDataSet.push(myArray);
+    });
+
+**Step 5** - Push the data to the table in tabletop_feed.js.
+
+    $('#example').dataTable( {
+	"aaData": newDataSet,
+	"aoColumns": tableColumnSet
+    });
---
+++ scripts/tabletop_feed.js
@@ -0,0 +1,55 @@
+	var urlStart = 'https://docs.google.com/spreadsheet/pub?';
+	var urlKey = 'key=0An8W63YKWOsxdHVreXpLbVRWUGlJUlcweHVfZ01ycVE&';
+	var urloutput = 'single=true&gid=2&output=html';
+	var public_spreadsheet_url = 	urlStart + urlKey + urloutput;
+	var newDataSet = [];
+
+	//set up your DataTables column headers.
+	var tableColumnSet =   [
+		{ "sTitle": "Name", "sClass": "center"},
+		{ "sTitle": "Website", "sClass": "center"},
+		{ "sTitle": "City", "sClass": "center"}
+	];
+
+	$(document).ready( function() {
+		Tabletop.init( { key: public_spreadsheet_url,
+			callback: showInfo,
+			simpleSheet: false,
+			debug: true } )
+	});
+
+	function showInfo(data, tabletop) {
+
+		$.each( tabletop.sheets(), function(i, sheet) {
+			$("#table_info").append("<p>Your sheet named " + sheet.name + " has " + sheet.column_names.join(", ") + " as columns that you can pull data from.</p>");
+		});
+
+		// access the data from your spreadsheet
+		$.each( tabletop.sheets("Active Breweries").all(), function(i, brewery) {
+			var nameData = brewery.name;
+			var websiteData = brewery.website;
+			var cityData = brewery.city;
+			var myArray = [nameData, websiteData, cityData]
+
+			newDataSet.push(myArray);
+		});
+
+			console.log(newDataSet);
+
+		$('#demo').html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"></table>' );
+
+		// push the data to the table
+		$('#example').dataTable( {
+			"aaData": newDataSet,
+			"aoColumns": tableColumnSet
+		});
+	}
+
+		//define two custom functions (asc and desc) for string sorting
+		jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
+			return ((x < y) ? -1 : ((x > y) ?  0 : 0));
+		};
+
+		jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
+			return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+		};
