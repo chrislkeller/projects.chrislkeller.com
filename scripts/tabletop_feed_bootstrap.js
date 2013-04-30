35e77888a0323054a1e54fe74bee101041ddfeca
Chris Keller <christopherlawrencekeller@gmail.com>
2013-04-30 12:54:44 -0700

    updates scripts/tabletop_feed_bootstrap.js with streamlined, non-pasta version I originally wrote

--- scripts/tabletop_feed_bootstrap.js
+++ scripts/tabletop_feed_bootstrap.js
@@ -1,60 +1,58 @@
-	var urlStart = 'https://docs.google.com/spreadsheet/pub?';
-	var urlKey = 'key=0As3JvOeYDO50dF9NWWRiaTdqNmdKQ1lCY3dpdDhZU3c&';
-	var urloutput = 'single=true&grid=2&output=html';
-	var public_spreadsheet_url = urlStart + urlKey + urloutput;
-	var newDataSet = [];
+var jqueryNoConflict = jQuery;
 
-	//set up your DataTables column headers.
-	var tableColumnSet =   [
-		{ "sTitle": "Name", "sClass": "center"},
-		{ "sTitle": "URL", "sClass": "center"},
-		{ "sTitle": "More Info", "sClass": "center"}
+// begin main function
+jqueryNoConflict(document).ready(function(){
+
+    initializeTabletopObject('0As3JvOeYDO50dF9NWWRiaTdqNmdKQ1lCY3dpdDhZU3c');
+
+});
+
+// pull data from google spreadsheet
+function initializeTabletopObject(dataSpreadsheet){
+    Tabletop.init({
+        key: dataSpreadsheet,
+        callback: writeTableWith,
+        simpleSheet: true,
+        debug: false
+    });
+}
+
+// create table headers
+function createTableColumns(){
+
+    /* swap out the properties of mDataProp & sTitle to reflect
+    the names of columns or keys you want to display.
+    Remember, tabletop.js strips out spaces from column titles, which
+    is what happens with the More Info column header */
+    var tableColumns =   [
+		{'mDataProp': 'name', 'sTitle': 'Name', 'sClass': 'center'},
+		{'mDataProp': 'url', 'sTitle': 'URL', 'sClass': 'center'},
+		{'mDataProp': 'moreinfo', 'sTitle': 'More Info', 'sClass': 'center'}
 	];
+    return tableColumns;
+}
 
-	$(document).ready( function() {
-		Tabletop.init( { key: public_spreadsheet_url,
-			callback: showInfo,
-			simpleSheet: false,
-			debug: true } )
-	});
+// create the table container and object
+function writeTableWith(dataSource){
 
-	function showInfo(data, tabletop) {
+    jqueryNoConflict('#demo').html('<table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered table-striped" id="data-table-container"></table>');
 
-		$.each( tabletop.sheets(), function(i, sheet) {
-			$("#table_info").append("<p>Your sheet named " + sheet.name + " has " + sheet.column_names.join(", ") + " as columns that you can pull data from.</p>");
-		});
+    var oTable = jqueryNoConflict('#data-table-container').dataTable({
+		'sPaginationType': 'bootstrap',
+		'iDisplayLength': 25,
+        'aaData': dataSource,
+        'aoColumns': createTableColumns(),
+        'oLanguage': {
+            'sLengthMenu': '_MENU_ records per page'
+        }
+    });
+};
 
-		// access the data from your spreadsheet
-		$.each( tabletop.sheets("Breweries").all(), function(i, brewery) {
-			var nameData = brewery.name;
-			var websiteData = brewery.url;
-			var cityData = brewery.moreinfo;
-			var myArray = [nameData, websiteData, cityData]
+//define two custom functions (asc and desc) for string sorting
+jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
+	return ((x < y) ? -1 : ((x > y) ?  0 : 0));
+};
 
-			newDataSet.push(myArray);
-		});
-
-			console.log(newDataSet);
-
-		$('#table_div').html( '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="table"></table>' );
-
-		// push the data to the table
-		$('#table').dataTable( {
-			"sPaginationType": "bootstrap",
-			"iDisplayLength": 25,
-			"aaData": newDataSet,
-			"aoColumns": tableColumnSet,
-			"oLanguage": {
-				"sLengthMenu": "_MENU_ records per page"
-			}
-		});
-	}
-
-		//define two custom functions (asc and desc) for string sorting
-		jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
-			return ((x < y) ? -1 : ((x > y) ?  0 : 0));
-		};
-
-		jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
-			return ((x < y) ?  1 : ((x > y) ? -1 : 0));
-		};
+jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
