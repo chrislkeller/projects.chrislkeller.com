ba3746d12b58a51f3e2f409d826148045ffe95d4
chrislkeller <christopherlawrencekeller@gmail.com>
2013-06-02 14:41:26 -0700

    fixes modal issues

--- static-files/scripts/data-script.js
+++ static-files/scripts/data-script.js
@@ -26,5 +26,27 @@
         handlebarsDebugHelper();
         renderHandlebarsTemplate('static-files/templates/data-visuals.handlebars', '#data-visuals', handlebarsData);
 
+        // fix background for mobile devices
+        if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+
+        }
+
+    };
+    // end
+
+    // when user submit button is clicked
+    function userSubmit(){
+        jqueryNoConflict('#content-article-text').hide();
+        jqueryNoConflict('#content-article-buttons').hide();
+        jqueryNoConflict('#data-user-submit').show();
+        renderHandlebarsTemplate('static-files/templates/data-user-submit.handlebars', '#data-user-submit');
+    };
+    // end
+
+    // return to main view
+    function cancelSubmit(){
+        jqueryNoConflict('#content-article-text').show();
+        jqueryNoConflict('#content-article-buttons').show();
+        jqueryNoConflict('#data-user-submit').hide();
     };
     // end
