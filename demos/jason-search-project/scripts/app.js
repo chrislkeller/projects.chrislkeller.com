ae3a0743fc634e0c1b4c6682a104ff5a1e7aa5bb
Chris Keller <christopherlawrencekeller@gmail.com>
2014-12-08 22:39:03 -0800

    moves mailchimp script to bottom of the page to check load time

--- demos/jason-search-project/scripts/app.js
+++ demos/jason-search-project/scripts/app.js
@@ -7,20 +7,6 @@
         Router: {},
     };
 
-    // load external templates via ajax
-    window.template = function(url){
-        var data = "<h1> failed to load url : " + url + "</h1>";
-        $.ajax({
-            async: false,
-            dataType: "text",
-            url: url,
-            success: function(response) {
-                data = response;
-            }
-        });
-        return data;
-    };
-
     $(function(){
         window.app = new App.Router();
         Backbone.history.start({
@@ -28,53 +14,6 @@
             pushState: false,
         });
     });
-
-    // helper functions
-    window.percentifyValue = function(value){
-        var value = value * 100
-        return parseFloat(value.toFixed(2));
-    };
-
-    window.toFixedPercent = function(part, whole){
-        var targetValue = part / whole;
-        var decimal = parseFloat(targetValue);
-        return decimal
-    };
-
-    window.addCommas = function(nStr){
-        nStr += "";
-        x = nStr.split(".");
-        x1 = x[0];
-        x2 = x.length > 1 ? "." + x[1] : "";
-            var rgx = /(\d+)(\d{3})/;
-                while (rgx.test(x1)) {
-                    x1 = x1.replace(rgx, "$1" + "," + "$2");
-                }
-            return x1 + x2;
-    };
-
-    window.ifEmptyStringForTotal = function(value){
-        var result;
-        if (value === ""){
-            result = "Total not available";
-        } else {
-            result = window.addCommas(value);
-        }
-        return result;
-    };
-
-    String.prototype.truncateToGraf = function(){
-        var lengthLimit = 900;
-        if (this.length > lengthLimit){
-            return this.substring(0, lengthLimit) + " ... ";
-        } else {
-            return this;
-        }
-    };
-
-    String.prototype.toProperCase = function(){
-        return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    };
 
 })();
 
