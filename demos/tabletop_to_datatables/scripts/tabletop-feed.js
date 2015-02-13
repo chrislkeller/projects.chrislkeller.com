2ce30526fd3f8e8668e814fd5cb77f3015c7f070
Chris Keller <christopherlawrencekeller@gmail.com>
2015-02-12 19:03:50 -0800

    updates to the tabletop_to_datatables demo project script to link cell to website

--- demos/tabletop_to_datatables/scripts/tabletop-feed.js
+++ demos/tabletop_to_datatables/scripts/tabletop-feed.js
@@ -3,7 +3,7 @@ var jqueryNoConflict = jQuery;
 // begin main function
 jqueryNoConflict(document).ready(function(){
 
-    initializeTabletopObject('https://docs.google.com/spreadsheets/d/0An8W63YKWOsxdHlyTVAzQ1RPSGhxZDM5T3NqdU9RcHc/pubhtml');
+    initializeTabletopObject("https://docs.google.com/spreadsheets/d/0An8W63YKWOsxdHlyTVAzQ1RPSGhxZDM5T3NqdU9RcHc/pubhtml");
 
 });
 
@@ -26,9 +26,9 @@ function createTableColumns(){
     is what happens with the More Info column header */
 
     var tableColumns =   [
-		{'mDataProp': 'brewery', 'sTitle': 'Brewery', 'sClass': 'center'},
-		{'mDataProp': 'city', 'sTitle': 'City', 'sClass': 'center'},
-		{'mDataProp': 'website', 'sTitle': 'Website', 'sClass': 'center'}
+		{"mDataProp": "brewery", "sTitle": "Brewery", "sClass": "center"},
+		{"mDataProp": "city", "sTitle": "City", "sClass": "center"},
+		{"mDataProp": "website", "sTitle": "Website", "sClass": "center"}
 	];
     return tableColumns;
 }
@@ -36,24 +36,30 @@ function createTableColumns(){
 // create the table container and object
 function writeTableWith(dataSource){
 
-    jqueryNoConflict('#demo').html('<table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered table-striped" id="data-table-container"></table>');
+    jqueryNoConflict("#demo").html("<table cellpadding='0' cellspacing='0' border='0' class='display table table-bordered table-striped' id='data-table-container'></table>");
 
-    var oTable = jqueryNoConflict('#data-table-container').dataTable({
-		'sPaginationType': 'bootstrap',
-		'iDisplayLength': 25,
-        'aaData': dataSource,
-        'aoColumns': createTableColumns(),
-        'oLanguage': {
-            'sLengthMenu': '_MENU_ records per page'
+    var oTable = jqueryNoConflict("#data-table-container").dataTable({
+        "sPaginationType": "bootstrap",
+        "iDisplayLength": 25,
+        "aaData": dataSource,
+        "aoColumns": createTableColumns(),
+        "fnRowCallback": function(nRow, aData, iDisplayIndex) {
+            console.log(aData);
+            $("td:eq(2)", nRow).html("<a href='http://" + aData.website + "'>Website</a>");
+            return nRow;
+        },
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ records per page"
         }
     });
+
 };
 
 //define two custom functions (asc and desc) for string sorting
-jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
+jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
 	return ((x < y) ? -1 : ((x > y) ?  0 : 0));
 };
 
-jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
+jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
 	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
 };
